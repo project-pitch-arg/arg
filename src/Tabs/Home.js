86d206a910a03485d586065917c83aebebaf2cbe
Karim-El-Nahass <karimnah01@hotmail.com>
2022-02-10 13:56:12 +0100

    Added Empty Template website

---
+++ src/Tabs/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './TabContent.css';
+
+export default function Walk() {
+    return (
+        <div class="content">
+          <a>Content for <a class="tab">Home</a> Tab <br/> goes here... </a>          
+        </div>
+      
+    )
+}
