c48d30c074d8f90b1b46c3076000a6e3fb54ab40
Sara Stråhle <sarstr@student.chalmers.se>
2022-02-12 13:38:14 +0100

    Moved some more things.
    To only have on copy of the node_modules on your local computer put it in "Webpages", the same level as "Bashemsida" and "Blog".

---
+++ Webpages/Blog/src/App.js
@@ -0,0 +1,34 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import TopBar from './TopBar/TopBar';
+import Wrong from './Wrong';
+import Home from './Tabs/Home';
+import TabTwo from './Tabs/TabTwo';
+import TabThree from './Tabs/TabThree';
+// import TabFour from './Tabs/TabFour';
+import TabFourOne from './Tabs/DropDown/TabFourOne';
+import TabFourTwo from './Tabs/DropDown/TabFourTwo';
+import TabFourThree from './Tabs/DropDown/TabFourThree';
+import Footer from './Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <TopBar />
+        <Routes>
+          <Route path="*" element={<Wrong />} /> {/*Default when path doesn't match any of the ones listed below*/}
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/Two" element={<TabTwo />} />
+          <Route exact path="/Three" element={<TabThree />} />
+          {/* <Route exact path="/Four" element={<TabFour />} /> */}
+            <Route exact path="/FourOne" element={<TabFourOne />} />
+            <Route exact path="/FourTwo" element={<TabFourTwo />} />
+            <Route exact path="/FourThree" element={<TabFourThree />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ Webpages/Blog/src/Tabs/TabTwo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './TabContent.css';
+
+export default function Walk() {
+    return (
+        <div class="content">
+          <a>Content for <a class="tab">Second </a> tab <br/> goes here... </a>          
+        </div>
+      
+    )
+}
