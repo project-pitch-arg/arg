d33f964770ed2addf5fc6b97d067cca50ae5ee33
Sara Stråhle <sarstr@student.chalmers.se>
2022-02-13 14:01:21 +0100

    Beginning of making blog posts.

---
+++ Webpages/Blog/src/Tabs/PostContent.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './TabContent.css';
+import tree from '../img/TestBild.jfif';
+
+export default function PostContent() {
+  return (
+   [
+     {date: '13-02-22', name: "Jack", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+     {date: '13-02-22', name: "Jack", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+     {date: '13-02-22', name: "Jack", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+     {date: '13-02-22', name: "Jack", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+     {date: '13-02-22', name: "Jack", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+     {date: '13-02-22', name: "Jack", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+     {date: '13-02-22', name: "Jack", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+     {date: 2, name: "Mark", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit", image: Image()},
+     {date: 3, name: "Mike"},
+     {date: 4, name: "Russell"}
+   ]
+  )
+}
+
+function Image() {
+  return (
+    <div className="Bild">
+      <img src={tree} />
+    </div>
+  )
+}
--- Webpages/Blog/src/Tabs/TabThree.js
+++ Webpages/Blog/src/Tabs/TabThree.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import './TabContent.css';
+import PostContent from './PostContent.js';
 
 export default function Posts() {
-    return (
-      <div class="background">
-        <div class="post">
-         <p>Hello</p>
-        </div> 
+  return (
+      <div class="background">     
+         <div class="postlist">
+         {PostContent().map(item => {
+            return (
+          <div class="post">
+            <div class="post-date">
+              {item.date}
+            <div class="post-name"> 
+              {item.name} 
+            <div class="post-content">
+             {item.content}
+            </div>
+            <div class="post-img">
+             {item.image}
+            </div>  
+           </div>
+         </div>
        </div>
+      )
+      })}
+         </div> 
+      </div>
     )
 }
