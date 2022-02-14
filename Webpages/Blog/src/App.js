60c17c1ee4ed3880fcbda4ae5448fd8e9720d637
Ludvig Lemner <lemnerl@student.chalmers.se>
2022-02-14 14:06:45 +0100

    Chat widget for blog
    Added a chat widget for the blog page.
    It's customisable and currently has the function of being expandable sideways as well as have an automated response on opening the chat as well as responding to input.

--- Webpages/Blog/src/App.js
+++ Webpages/Blog/src/App.js
@@ -1,3 +1,4 @@
+import React, {useEffect} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TopBar from './TopBar/TopBar';
 import Wrong from './Wrong';
@@ -10,10 +11,39 @@ import TabFourTwo from './Tabs/DropDown/TabFourTwo';
 import TabFourThree from './Tabs/DropDown/TabFourThree';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
+import { Widget, addResponseMessage } from 'react-chat-widget';
+import 'react-chat-widget/lib/styles.css';
+
+import logo from './logo.svg';
 
 function App() {
+  useEffect(() => {
+    addResponseMessage('Welcome, thank you for reading this');
+  }, []);
+
+  const handleNewUserMessage = (newMessage) => {
+    console.log(`New message incoming! ${newMessage}`);
+    //Check this link for message details https://www.npmjs.com/package/react-chat-widget#messages
+    addResponseMessage("This service is temporarily unavailable");
+  };
   return (
+    
     <div className="App"> 
+      <Widget 
+        //Check https://www.npmjs.com/package/react-chat-widget#API for more API props
+        handleNewUserMessage={handleNewUserMessage}
+        senderPlaceHolder="Write damn you!"
+        profileAvatar={logo}
+        profileClientAvatar={null}
+        titleAvatar={null}
+        fullScreenMode={false}
+        showCloseButton={true}
+        showTimeStamp={false} //Temporary to be able to use old messages
+        resizable={true}
+        emojis={true}
+        title="Widget chat for blog"
+        subtitle="Still a work in progress"
+      />
       <BrowserRouter>
        <Header />
         <TopBar />
