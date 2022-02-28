1feb869232d367b295a735dd26d90404fd6a078d
Oscar Forsyth <44546586+goatlover@users.noreply.github.com>
2022-02-28 11:57:50 +0100

    started working on about pages

--- src/Tabs/AboutUs.js
+++ src/Tabs/AboutUs.js
@@ -4,12 +4,26 @@ import { useEffect } from 'react';
 
 import teamMember from '../img/team-member.png';
 
+import aboutUsImg from '../img/about-us.jpg';
+import environmentImg from '../img/environment.jpg';
+import goalsImg from '../img/goals.jpg';
+
+import christianImg from '../img/christian-yates.jpg';
+import eloiseImg from '../img/eloise-christensen.jpg';
+import georgeImg from '../img/george-caroll.jpg';
+import lyraImg from '../img/lyra-bell.jpg';
+import madeleineImg from '../img/madeleine-baldwin.jpg';
+import manuelImg from '../img/manuel-mallory.jpg';
+import markImg from '../img/mark-roswell.jpg';
+import summerImg from '../img/summer-morton.jpg';
+
+
 export default function AboutUs() {
     
     useEffect(() => {
       window.scrollTo(0, 0)
     }, [])
-  
+  /*
     return (
         <div>
           <div class="header">
@@ -26,5 +40,58 @@ export default function AboutUs() {
           </div>
         </div>
       
+    )
+    */
+    return(
+
+        <div>
+          <div class="header" style={{backgroundImage: `url(${environmentImg})`}}>
+            <a>About Us</a>
+          </div>
+
+          <div class="content">
+
+            <div id="intro-content">
+              <div id="intro-img">
+                <img src={goalsImg} height='215px'/>
+              </div>
+              <div id="intro-desc">
+                <h1> Our Goals </h1>
+                <a> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </a>
+              </div>
+            </div>
+
+            <div id="goal-content">
+              <div id="goal-desc">
+                <h1>Sustainable Practices</h1>
+                <a>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </a>
+              </div>
+              <div id="goal-img"><img src={environmentImg}/></div>
+            </div>
+            <div id="tech-content">
+              <h1>Why Work with us?</h1>
+              <a>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut</a>
+            </div>
+          </div>
+
+          <div class="content">
+
+              <div id="team-content">
+                <h1>Who are we?</h1>
+                <div class="team-member"><img src={markImg}/><a>Mark Roswell</a></div>
+                <div class="team-member"><img src={manuelImg}/><a>Manuel Mallory</a></div>
+                <div class="team-member"><img src={georgeImg}/><a>George Caroll</a></div>
+                <div class="team-member"><img src={christianImg}/><a>Christian Yates</a></div>
+              </div>
+
+              <div id="team-content">
+                  <div class="team-member"><img src={summerImg}/><a>Summer Morton</a></div>
+                  <div class="team-member"><img src={madeleineImg}/><a>Madeleine Baldwin</a></div>
+                  <div class="team-member"><img src={lyraImg}/><a>Lyra Bell</a></div>
+                  <div class="team-member"><img src={eloiseImg}/><a>Eloise Christensen</a></div>
+               </div>
+          </div>
+        </div>
+
     )
 }
