cdd4a7ef5c29a26b20643f8e0fbe06a18f805f16
Sara Stråhle <sarstr@student.chalmers.se>
2022-03-11 13:44:05 +0100

    More comments in chat

--- Webpages/chat/src/StartUp.py
+++ Webpages/chat/src/StartUp.py
@@ -9,7 +9,7 @@
 # go to the corresponding JSON-files listed
 # below.
 #
-# JSON-files
+# JSON-files:
 # users.json: Edit what users to crete.
 # StartChats.json: Edit which chatroomsexist and
 #                  which users has access.                   
@@ -19,90 +19,121 @@ import json
 import requests
 import hashlib
 
-
+# Change these to your own project!
 PROJECT_ID = "bd7ed9ec-9d77-4413-a5d3-0ee995f30015"
 PRIVATE_KEY = "187bca6e-c11c-4815-9071-fbe87c45a3a6"
 
-
+# Create and array to save the ID:s of the
+# created chats.
+# Uses the format of [[chatName, chatId], [chatName, chatId]]
+# TODO Move this?
 chatIds = []
 
+# Find the corresponding chatId from 
+# the name. It takes the array of arrays
+# and the name of the chat as arguments
+# and returns the ID.
 def findChatId(chatArray, chatToFind):
   for chat in chatArray:
       if(chat[0] == chatToFind):
         return chat[1]
 
-#--------------- Create users ---------------
+#--------------- Create users --------------
 
+# Load values from file
 f = open('users.json',)
 users = json.load(f)
 f.close()
 
+# Go through all the users to be created
 for user in users:
+    
+    # TODO Rename this so it looks better.
+    # Fix the format of the JSON-date so that
+    # the password can be hashed.
     users = str(user).replace("'", "\"")
     users2 = str(users).replace("None", "null")
     json_object = json.loads((users2))
     json_object["secret"] = hashlib.sha256(str(json_object["secret"]).encode('utf-8')).hexdigest()
 
+    # Send the request to create a user with
+    # payload and header.
     r = requests.post(
         'https://api.chatengine.io/users/',
         data=json_object,
         headers={"Private-Key": PRIVATE_KEY}
     )
+    # Print the result of the sent post request.
     print(r.status_code)
-
 
 #--------------- Create chats ---------------
 
+# Load values from file.
 f = open('StartChats.json',)
 chats = json.load(f)
 f.close()
 
-
+# Go through all the chats to be created.
 for chat in chats:
+    
+    # Create a JSON-object with the chatname.
     title = "{\"title\":" + "\"" + chat["chat-name"] + "\"" + "}"
     title2 = json.loads(title)
     
+    # Send the request to create a chat with payload and header.
     r = requests.post(
         'https://api.chatengine.io/chats/',
         data=title2,
         headers={"Private-Key": PRIVATE_KEY, "User-Name": chat["admin"], "User-Secret": chat["admin-secret"]}
     )
+    # Print the result of the sent post request.
     print(r.status_code)
 
+    # Add the id for the chat to the list of chat ids.
     getChatId = json.loads(r.text)
     chatId = str(getChatId["id"])
-
     chatIds.append([chat["chat-name"], chatId])
 
+    # Get the password for the admin for this chat from the file and hash it.
     password = chat["admin-secret"]
     password = hashlib.sha256(password.encode('utf-8')).hexdigest()
 
+    # Iterate the users that will be added to the newly created chat.
     for user in chat["users"]:
-
+        
+        # Create a JSON-object for the user to be added.
         user = json.loads("{\"username\":" +  "\"" + user  +"\""  + "}")
 
+        # Send the request to add a user with payload and header.
         r = requests.post(
             'https://api.chatengine.io/chats/' + chatId + '/people/',
             data=user,
             headers={"Project-ID": PROJECT_ID, "User-Name": chat["admin"], "User-Secret": password}
         )
+    # Print the result of the sent post request.
     print(r.status_code)
 
-#--------------- Print posts in chats ---------------
+#--------------- Print messages in chats ---------------
 
-f = open('ChatPosts.json',)
-posts = json.load(f)
+# Load values from file
+f = open('ChatMessages.json',)
+messages = json.load(f)
 f.close()
 
-for post in posts:
+# Iterate through all messages to send
+for message in messages:
 
-    chatID = findChatId(chatIds, post["chat-name"])
+    # Find the ID of the chat to send the message in
+    # to have the right address for the server request.
+    chatID = findChatId(chatIds, message["chat-name"])
 
+    # Send the request to post a message with payload and header.
     r = requests.post(
         'https://api.chatengine.io/chats/' + chatID  +'/messages/',
-        data=post,
-        headers={"Private-Key": PRIVATE_KEY, "User-Name": post["user"], "User-Secret": post["password"]}
+        data=message,
+        headers={"Private-Key": PRIVATE_KEY, "User-Name": message["user"], "User-Secret": message["password"]}
     )
+    # Print the result of the post request.
     print(r.status_code)
 
 
--- Webpages/chat/src/changeableVariables.jsx
+++ Webpages/chat/src/changeableVariables.jsx
@@ -7,6 +7,7 @@
     here will give you the required
     project ID and private key used in this application.
     Please edit PROJECT_ID and PRIVATE_KEY to use your keys.
+    Also, change them in StartUp.py.
 
     For the JSON-files below, you can find their corresponding
     shcemas ???, ??? and ??? in the folder for docuementaiton
@@ -35,6 +36,8 @@ export const PRIVATE_KEY = "187bca6e-c11c-4815-9071-fbe87c45a3a6";
 // The chat rooms a new user will be entered into.
 // The numbers can be found on your web page.
 // It would be good to edit this when all chosen default chats are created.
+// If you want to enter users into the chats with ID:s 1,2 and 3
+// the array would be [1, 2, 3].
 export const DEFAULT_CHATS = [102138];
 
 // The default users existing in the chat.
