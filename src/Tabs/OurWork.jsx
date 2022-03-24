ad9ca513ec73d7b7748095e3b04107806a8738f0
Karim-El-Nahass <karimnah01@hotmail.com>
2022-03-24 11:54:21 +0100

    Added a puzzle

--- src/Tabs/OurWork.jsx
+++ src/Tabs/OurWork.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import {Slide} from 'react-slideshow-image';
 import './Company.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import header from '../img/work-header.jpg'
 import programming from '../img/programming.jpg'
 
-import slideImage1 from '../img/nobel-prize.jpg';
-import slideImage2 from '../img/tablet.png';
-import slideImage3 from '../img/smartphone.jpg';
+import slideImage1 from '../img/slideImage1.jpg';
+import slideImage2 from '../img/slideImage2.jpg';
+import slideImage3 from '../img/slideImage3.jpg';
+import slideImage4 from '../img/slideImage4.jpg';
 
 import futurePlans1 from '../img/future-ai.jpg'
 import futurePlans2 from '../img/space-shuttle.jpg'
@@ -18,7 +19,32 @@ export default function OurWork() {
   
     useEffect(() => {
       window.scrollTo(0, 0)
-    }, [])
+    }, []);
+
+    const [countFirst, setCountFirst]   = useState(0);
+    const [countSecond, setCountSecond] = useState(0);
+    const [countThird, setCountThird]   = useState(0);
+    const [countFourth, setCountFourth] = useState(0);
+
+    const firstVar  = 8;
+    const secondVar = 9;
+    const thirdVar  = 2;
+    const fourthVar = 6;
+
+    // console.log("First: " + countFirst + ", Second: " + countSecond + ", Third: " + countThird + ", Fourth: " + countFourth);
+    
+    var codeCracked = false;
+    if (countFirst === firstVar && countSecond === secondVar && countThird === thirdVar && countFourth === fourthVar) {
+      codeCracked = true;
+    }
+
+    if (codeCracked) {
+      console.log(617264);
+      setTimeout(function() {
+        console.clear();
+      }, 2000)
+    } 
+
 
     return (
         <div>
@@ -29,50 +55,56 @@ export default function OurWork() {
           <div class="content">
             <div id="current-content">
               <div id="current-img">
-                <img src={programming} height='215px'/>
+                <img src={programming} alt="" height='215px'/>
               </div>
               <div id="current-desc">
-                <h1> What we do </h1>
-                <a> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </a>
+                <h1> Current work </h1>
+                <a> Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis alias sunt fugiat, repudiandae vel ea veritatis nihil et nobis aperiam officia voluptates accusamus at excepturi est! Quam harum earum tenetur.</a>
               </div>
             </div>
 
             <div id="past-content">
                 <div id="past-desc">
                 <h1>What we have done</h1>
-                <a>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Aliquam faucibus purus in massa tempor nec feugiat nisl pretium. Vel eros donec ac odio. Id aliquet risus feugiat in ante metus dictum. Faucibus et molestie ac feugiat sed. Mattis vulputate enim nulla aliquet porttitor. Amet cursus sit amet dictum sit amet justo donec. Ut faucibus pulvinar elementum integer enim neque volutpat ac tincidunt. Magna ac placerat vestibulum lectus mauris ultrices eros in. Orci phasellus egestas tellus rutrum tellus pellentesque. Volutpat est velit egestas dui id ornare arcu odio ut. Ornare lectus sit amet est placerat in. Nibh sed pulvinar proin gravida. Habitant morbi tristique senectus et netus. Habitasse platea dictumst vestibulum rhoncus est. Eu turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Donec et odio pellentesque diam volutpat commodo sed. Posuere morbi leo urna molestie at elementum eu facilisis sed. Quam id leo in vitae. Iaculis urna id volutpat lacus laoreet non curabitur gravida arcu. </a>
+                <a> We have made it possible for our customers to stay securly connected wherever they may be. This was done using the modern technologies as well as AI's in order to obtain ultimate security, which is important for us. Examples on where this has been implemented are in drones, smartphones, smartwatches and even Virtual Reality (VR). 
+                </a>
               </div>
 
             
-            <div id="past-slideshow">
-              <Slide easing="ease" autoplay={true} indicators={true}>
+              <div id="slideshow-container">
+              <Slide easing="ease" autoplay={false} indicators={true}>
               <div class="slide-img">
-                <div style={{'backgroundImage': `url(${slideImage1})`}}>
-                  <span>Nobel Prize</span>
+                <div onClick={() => setCountFirst(countFirst + 1)} style={{'backgroundImage': `url(${slideImage1})`}}>
+                  <span>{firstVar}</span>
                 </div>
               </div>
               <div class="slide-img">
-                <div style={{'backgroundImage': `url(${slideImage2})`}}>
-                  <span>Tablet</span>
+                <div onClick={() => setCountSecond(countSecond + 1)} style={{'backgroundImage': `url(${slideImage2})`}}>
+                  <span>{secondVar}</span>
                 </div>
               </div>
               <div class="slide-img">
-                <div style={{'backgroundImage': `url(${slideImage3})`}}>
-                  <span>Smartphone</span>
+                <div onClick={() => setCountThird(countThird + 1)} style={{'backgroundImage': `url(${slideImage3})`}}>
+                  <span>{thirdVar}</span>
+                </div>
+              </div>
+              <div class="slide-img">
+                <div onClick={() => setCountFourth(countFourth + 1)} style={{'backgroundImage': `url(${slideImage4})`}}>
+                  <span>{fourthVar}</span>
                 </div>
               </div>
               </Slide>
-            </div>  
+            </div>
           </div>
             
             <div id="future-content">
               <h1>Future Plans</h1>
-              <a>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut</a>
+              <a>For future plans, we plan to reach the sky with our technology and security. Which can happen by the use of artifical intelligent and advanced programming with help from our professional programmers. We plan on broadening our research area even more to reach every aspect of it and obtain complete security.</a>
               <div id="future-left">
-                <div id="future-left-top"><img src={futurePlans3} height='100%' width='100%'/></div>
-                <div id="future-left-bottom"><img src={futurePlans1} height='100%' width='100%'/></div>
+                <div id="future-left-top"><img src={futurePlans3} alt="" height='100%' width='100%'/></div>
+                <div id="future-left-bottom"><img src={futurePlans1} alt="" height='100%' width='100%'/></div>
               </div>
-              <div id="future-right"><img src={futurePlans2} height='100%' width='100%'/></div>
+              <div id="future-right"><img src={futurePlans2} alt="" height='100%' width='100%'/></div>
             </div>
 
           </div>
