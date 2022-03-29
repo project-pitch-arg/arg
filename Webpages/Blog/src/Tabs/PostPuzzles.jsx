db5b46d0ee70b55e80679b709e980870f23b79e7
Ludvig Lemner <lemnerl@student.chalmers.se>
2022-03-29 12:39:30 +0200

    Update PostPuzzles.jsx

--- Webpages/Blog/src/Tabs/PostPuzzles.jsx
+++ Webpages/Blog/src/Tabs/PostPuzzles.jsx
@@ -157,8 +157,8 @@ export default function Puzzles(post) {
           </div>
           <div>
             <picture>
-              <source media="(min-width: 800px)" srcSet="PostImages/RedSkyAndPool.JPG" class="post-image"/> 
-              <img src="" alt="20 steps 34N, 25 steps 150SE, 115 steps 71E, 88 steps 336NW, 72 steps 354N, 35 steps 53NE, 60 steps 6N, 60 steps 310NW, 55 steps 33NE, 80 steps 14N, 24 steps 73E, 125 steps 116SE, 62 steps 84E, 112 steps 16N, 155 steps 18N, 30 steps 260W, 34 steps 303NW, 73 steps 280W, 227 steps 104E, 110 steps 351N, 5 steps 38NE, 25 steps 155SE, F23" class="post-image"/>
+              <source media="(min-width: 500px)" srcSet="PostImages/RedSkyAndPool.JPG" class="post-image"/> 
+              <img src="" alt="20 34N, 25 150SE, 115 71E, 88 336NW, 72 354N, 35 53NE, 60 6N, 60 310NW, 55 33NE, 80 14N, 24 73E, 125 116SE, 62 84E, 112 16N, 155 18N, 30 260W, 34 303NW, 73 280W, 227 104E, 110 351N, 5 38NE, 25 155SE, F23" class="post-image"/>
             </picture>
           </div>
       </div>  
