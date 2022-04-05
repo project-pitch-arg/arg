05076590c5a9ce94c0e9531a9b5204d8996d163b
William Andersson <williama.andersson@gmail.com>
2022-04-05 11:33:29 +0200

    Uppdaterat emailListener

--- Webpages/internFöretagsHemsida/server/emailListener.js
+++ Webpages/internFöretagsHemsida/server/emailListener.js
@@ -67,7 +67,7 @@ function sendMail(subject, to,message){
 }
 mailListener.on("mail", function(mail, seqno, attributes){
 
-    if((mail.subject.includes("code") && mail.subject.includes("241")) || (mail.text.includes("code") && mail.text.includes("241"))){
+    if(mail.text.includes("code") && mail.text.includes("241")){
         sendMail("Hello", mail.from, "Where did you get this information? \nI will check with my boss and return to you. \n//Helena");
         setTimeout(function() {
             sendMail("Hello", mail.from, "I talked with my boss and he agreed to talk? \n Here is his email. \n//Helena")
