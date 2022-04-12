845f94561798e80fe9d439f955d2494de41c4daf
Sara Stråhle <sarstr@student.chalmers.se>
2022-04-12 11:11:45 +0200

    Built-in validation for JSON-files in the startup for the chat

--- Webpages/chat/src/StartUp.py
+++ Webpages/chat/src/StartUp.py
@@ -16,12 +16,61 @@
 # ChatPosts.json: Edit the posts for the chats.
 
 import json
+import jsonschema
+from jsonschema import validate
 import requests
 import hashlib
+import sys
 
 # Change these to your own project!
 PROJECT_ID = "aa2bb056-425d-4756-bb63-ab5b02585256"
 PRIVATE_KEY = "ede1ace3-6939-44bf-9df8-c818978fc030"
+
+# Schemas for JSON-files.
+usersSchema = {
+    "type": "array",
+    "items": {
+        "type": "object",
+        "required": [ "username", "secret", "email" ],
+        "properties": {
+            "username": { "type": "string" },
+            "secret": { "type": "string" },
+            "email": { "type": "string"} },
+        "additionalProperties": False}
+    }
+
+startChatsSchema = {
+    "type": "array",
+    "items": {
+        "type": "object",
+        "required": [ "admin", "chat_name", "users" ],
+        "properties": {
+            "admin": { "type": "string" },
+            "chat_name": { "type": "string" },
+            "users": { "type": "array", "items": { "type": "string" } } }, 
+    "additionalProperties": False }
+    }
+
+chatMessageSchema = {
+    "type": "array",
+    "items": {
+        "type": "object",
+        "required": [ "text", "user" ],
+        "properties": {
+            "chat_name": { "type": "string" },
+            "text": { "type": "string" },
+            "user": { "type": "string" } },
+        "additionalProperties": False }
+    }
+
+# Function to validate JSON data with a schema.
+def validateJson(jsonData, jsonSchema):
+    try:
+        validate(instance=jsonData, schema=jsonSchema)
+    except jsonschema.exceptions.ValidationError as err:
+        return False
+    return True
+
 
 # Create and array to save the ID:s of the
 # created chats.
@@ -42,13 +91,43 @@ def findUserSecret(userArray, userName):
         if(user["username"] == userName):
             return user["secret"]
 
-#--------------- Create users --------------
-
-print("Creating users")
-# Load values from file
+# Load data from files and validate it.
+# Load users from file
 f = open('Users.json')
 accounts = json.load(f)
 f.close()
+
+# Validate the data.
+isValid = validateJson(accounts, usersSchema)
+
+if (not isValid):
+    sys.exit("ERROR: Given JSON data is Invalid. Change Users.json to follow the schema.")
+
+# Load startchats from file.
+f = open('StartChats.json',)
+chats = json.load(f)
+f.close()
+
+# Validate the data.
+isValid = validateJson(chats, startChatsSchema)
+
+if (not isValid):
+    sys.exit("ERROR: Given JSON data is Invalid. Change StartChats.json to follow the schema.")
+
+# Load messages from file
+f = open('ChatMessages.json',)
+messages = json.load(f)
+f.close()
+
+# Validate the data.
+isValid = validateJson(messages, chatMessageSchema)
+
+if (not isValid):
+    sys.exit("ERROR: Given JSON data is Invalid. Change ChatMessages.json to follow the schema.")
+    
+#--------------- Create users --------------
+
+print("Creating users")
 
 # Go through all the users to be created
 for user in accounts:
@@ -73,10 +152,6 @@ for user in accounts:
 #--------------- Create chats ---------------
 
 print("Creating chats")
-# Load values from file.
-f = open('StartChats.json',)
-chats = json.load(f)
-f.close()
 
 # Go through all the chats to be created.
 for chat in chats:
@@ -120,10 +195,6 @@ for chat in chats:
 #--------------- Print messages in chats ---------------
 
 print("Print messages in chats")
-# Load values from file
-f = open('ChatMessages.json',)
-messages = json.load(f)
-f.close()
 
 counter = 0
 currentChatID = -1
