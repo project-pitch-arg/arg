b9524a70a820f36ebb1fb958f8b2657ac20dc3e2
William Andersson <williama.andersson@gmail.com>
2022-04-14 21:03:25 +0200

    Fixat pdf läsare för emaillistener

--- src/InternalWebsite/server/emailListener.js
+++ src/InternalWebsite/server/emailListener.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const fs = require('fs');
+const PdfReader = require('pdfreader').PdfReader;
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 var Imap = require('imap'),
@@ -17,9 +20,9 @@ var mailListener = new MailListener({
   tlsOptions: { rejectUnauthorized: false },
   mailbox: "INBOX", // mailbox to monitor
   searchFilter: ["UNSEEN"], // the search filter being used after an IDLE notification has been retrieved
-  markSeen: true, // all fetched email willbe marked as seen and not fetched next time
+  markSeen: true, // all fetched email will be marked as seen and not fetched next time
   fetchUnreadOnStart: true, // use it only if you want to get all unread email on lib start. Default is `false`,
-  mailParserOptions: {streamAttachments: true}, // options to be passed to mailParser lib.
+  mailParserOptions: {streamAttachments: false}, // options to be passed to mailParser lib.
   attachments: true, // download attachments as they are encountered to the project directory
   attachmentOptions: { directory: "attachments/" } // specify a download directory for attachments
 });
@@ -48,16 +51,24 @@ var transporter = nodemailer.createTransport({
   }
 });
 var mailOptions = {
-  from: 'helena.godspeed@gmail.com',
-  to: 'fhemfhem55@gmail.com',
-  subject: 'Tjebbatjona',
-  text: 'Ditt lösenord är: ',
+  from: '',
+  to: '',
+  subject: '',
+  text: '',
 };
-function sendMail(subject, to,message){
-    mailOptions.subject = subject;
-    mailOptions.to = to;
-    mailOptions.text = message;
-    transporter.sendMail(mailOptions, function(error, info){
+
+function sendMail(subject,from, to, message){
+    var mailSettings = {
+      from: '',
+      to: '',
+      subject: '',
+      text: '',
+    };
+    mailSettings.from = from;
+    mailSettings.subject = subject;
+    mailSettings.to = to;
+    mailSettings.text = message;
+    transporter.sendMail(mailSettings, function(error, info){
         if (error) {
             console.log(error);
         } else {
@@ -67,28 +78,103 @@ function sendMail(subject, to,message){
 }
 mailListener.on("mail", function(mail, seqno, attributes){
     try {
-        if(mail.text.includes("231")){
-            sendMail("Hello", mail.from, "Where did you get this code? \n I will ask IT what it is about. \n//Helena");
+        if(mail.attachments !== undefined && mail.to[0].address === "wilfred@gmail.com"){
+            mailOptions.to = mail.from;
+            mailOptions.from = "wilfred@gmail.com";
+        }
+        else if(mail.text.includes("231") && mail.to[0].address === "helena.godspeed@gmail.com"){
+            sendMail("Hello", mail.to[0] , mail.from, "Where did you get this code? \n I will ask IT what it is about. \n//Helena");
             setTimeout(function() {
-                sendMail("Hello", mail.from, "I talked with IT and they say it's for a restricted page and we thank you for not sharing it.  \n//Helena")
+                sendMail("Hello",mail.to[0], mail.from, "I talked with IT and they say it's for a restricted page and we thank you for not sharing it.  \n//Helena")
             }, 60000);
         }
-        if(mail.text.includes("231")){
-                    sendMail("Hello", mail.from, "Where did you get this code? \n I will ask IT what it is about. \n//Helena");
-                    setTimeout(function() {
-                        sendMail("Hello", mail.from, "I talked with IT and they say it's for a restricted page and we thank you for not sharing it.  \n//Helena")
-                    }, 60000);
-                }
-        else {
-            sendMail("Hello", mail.from, "Hello, i'm out of the office for a two week vacation i will be back on the 1st of April \nBest Regards Helena :)");
+        else if(mail.text.includes("3186") && mail.to[0].address === "helena.godspeed@gmail.com"){
+            sendMail("Hello", mail.from, "Where did you get this code? \n I will ask IT what it is about. \n//Helena");
+            setTimeout(function() {
+                sendMail("Hello", mail.to[0], mail.from, "I talked with IT and they say it's for a restricted command and we thank you for not sharing it.  \n//Helena")
+            }, 60000);
+        }
+        else if(mail.to[0].address === "helena.godspeed@gmail.com"){
+            sendMail("Hello", mail.to[0], mail.from, "Hello, i'm out of the office for a two week vacation i will be back on the 1st of April \nBest Regards Helena :)");
+        }
+        else if(mail.to[0].address === "wilfred@gmail.com"){
+            sendMail("Hello", mail.to[0], mail.from, "Please use the code phrase");
         }
     }
     catch {
-        sendMail("Hello", mail.from, "Hello, i'm out of the office for a two week vacation i will be back on the 1st of April \nBest Regards Helena :)");
+        sendMail("Hello", mail.to[0], mail.from, "Hello, i'm out of the office for a two week vacation i will be back on the 1st of April \nBest Regards Helena :)");
     }
 
 });
+function readPDF(callback){
+    var dir = arguments[1];
+    fs.readdir(dir, function(err, filenames) {
+        if (err) {
+          onError(err);
+          return;
+        }
+        filenames.forEach(function(filename) {
+          fs.readFile (dir + filename, (err, pdfBuffer) => {
+          // pdfBuffer contains the file content
+          var text = "";
+          new PdfReader().parseBuffer(pdfBuffer, function(err, item){
+             if (err){
+                 onError(err);
+                 return;
+                 }
+              else if (!item){
+                callback(text, filenames.length);
+                return;
+              }
+              else if (item.text)
+                  text += item.text;
+             });
+          });
+        });
+    });
+}
+
+var totalText = "";
+var iText = 0;
+var secretText = "";
+var secretTextLoaded = false;
+var normalTextLoaded = false;
+
+function collectNormalTexts(text, filenames){
+    iText++;
+    totalText += text;
+    if(secretTextLoaded && filenames === iText){
+        compareTexts(totalText, secretText)
+    }
+    else if(filenames === iText){
+        normalTextLoaded = true;
+    }
+}
+
+function collectSecretTexts(text,filenames){
+    secretText += text;
+    secretTextLoaded = true;
+    if(normalTextLoaded){
+        compareTexts(totalText, secretText)
+    }
+}
+
+function compareTexts(text1, text2){
+    if(text1.includes(text2)){
+        sendMail("Hello", mailOptions.from, mailOptions.to, "This is exactly the information i need. Thank you so much for the help,  i don't know how to thank you :). \n//Wilfred");
+    }
+    else {
+        sendMail("Hello", mailOptions.from, mailOptions.to, "This is good information but i don't think it is enough, see if you could find out anything more. \n//Wilfred");
+    }
+    totalText = "";
+    secretText = "";
+}
 
 mailListener.on("attachment", function(attachment){
-  console.log(attachment.path);
+
+    var pathAttachments = path.join(__dirname, "/attachments/")
+    setTimeout(readPDF,20000,collectNormalTexts,pathAttachments);
+
+    var secretDirectory = path.join(__dirname, "/SecretFiles/")
+    setTimeout(readPDF, 20000, collectSecretTexts,secretDirectory);
 });
