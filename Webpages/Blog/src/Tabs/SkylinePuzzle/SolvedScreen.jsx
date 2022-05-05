d628847316f0bbaa9d331a1d12e7197a76ba161d
Ludvig Lemner <lemnerl@student.chalmers.se>
2022-05-05 20:16:18 +0200

    A bit of design for the new pages

--- Webpages/Blog/src/Tabs/SkylinePuzzle/SolvedScreen.jsx
+++ Webpages/Blog/src/Tabs/SkylinePuzzle/SolvedScreen.jsx
@@ -1,7 +1,18 @@
 export default function SolvedScreen(){
     return (
-        <div class="content">
-            Solved
+        <div className="content">
+            <div className="title-text">
+            Congratulations!
+            <br />
+            <br />
+            You solved it.
+            <br />
+            Your prize is the ability to change the color of the website. Send me (Robot1311213) a message in a private chat explaining the fact that you won and use the code "Triceratopses secretly live in Gothenburg University" to prove that you were the one who solved it.
+            <br />
+            <br />
+            
+            <img src={require("../../Img/Trophy.png")}/>
+            </div>
         </div>
     )
 }
